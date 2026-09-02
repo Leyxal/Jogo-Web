3261aac5245c891ffafd241e2d037df711d970b5
João Vitor <joaovitorsouzasena@gmail.com>
2026-09-02 15:51:46 -0400

    02/09/2026

---
+++ Projeto Web/sw.js
@@ -0,0 +1,35 @@
+// Service Worker para permitir instalação como PWA (Atalho na tela de início do celular)
+const CACHE_NAME = 'amazonia-bobble-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './js/game.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
